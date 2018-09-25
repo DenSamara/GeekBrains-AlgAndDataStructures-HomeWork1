9bc241fc730f27701869a3277c99024fec84d7bd
Denis Konovalov <den.konovaloff@gmail.com>
2018-09-25 14:58:07 +0400

    Task 4

--- Main.c
+++ Main.c
@@ -1,4 +1,6 @@
 #include "Main.h"
+
+#include <math.h>
 #include <stdio.h>
 
 int main(void)
@@ -9,6 +11,9 @@ int main(void)
 
 	//Task 2
 	int a = 1, b = 2, c = 9, d = 3, max;
+
+	//Task 4
+	float descr;
 
 	/*
 	//"¬вести вес и рост человека. –ассчитать и вывести индекс массы тела по формуле I=m/(h*h); где m-масса тела в килограммах, h - рост в метрах\n"
@@ -35,7 +40,6 @@ int main(void)
 	if (max < d) max = d;
 
 	printf("max = %d\n", max);
-	*/
 
 	//Ќаписать программу обмена значени€ми двух целочисленных переменных:
 	//a. с использованием третьей переменной;
@@ -57,8 +61,29 @@ int main(void)
 	a = a - b;
 
 	printf("a = %d, b = %d\n", a, b);
+	*/
 
-	scanf("%f", &h);
+	//4. Ќаписать программу нахождени€ корней заданного квадратного уравнени€
+	// (a*x)^2+b*x+c
+	// d = b^2-4*a*c, d > 0 (2 корн€), d = 0 - 1 корень, d < 0 - корней нет
+
+	printf("\n---=== 4 ===---\n");
+
+	a = 1, b = 5, c = 3;
+	printf("a = %d, b = %d, c = %d\n", a, b, c);
+	descr = b*b-4*a*c;
+
+	if (descr > 0){
+		float x1 = (-b+sqrt(descr))/2*a;
+		float x2 = (-b-sqrt(descr))/2*a;
+		printf("2 roots: x1 = %.2f, x2 = %.2f\n", x1, x2);
+	}else if (descr == 0){
+		float x = (-b-sqrt(descr))/2*a;
+		printf("1 root: x1 = %.2f\n", x);
+	}else{
+		printf("D = %f, no roots\n", descr);
+	}
+
 
 	return 0;
 }
