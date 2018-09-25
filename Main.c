7e449bfed3c1e661e0adb325ef7a971621082513
Denis Konovalov <den.konovaloff@gmail.com>
2018-09-25 13:48:17 +0400

    Максимум из 4 чисел, без использования массива

--- Main.c
+++ Main.c
@@ -3,11 +3,17 @@
 
 int main(void)
 {
-	printf("---=== 1 ===---\n");
-	printf("Ввести вес и рост человека. Рассчитать и вывести индекс массы тела по формуле I=m/(h*h); где m-масса тела в килограммах, h - рост в метрах\n");
-	int w, index;
-	float h;
+	//Task 1
+	int w;
+	float h, index;
 
+	//Task 2
+	int a = 1, b = 2, c = 9, d = 3, max;
+
+	/*
+	//"Ввести вес и рост человека. Рассчитать и вывести индекс массы тела по формуле I=m/(h*h); где m-масса тела в килограммах, h - рост в метрах\n"
+	printf("---=== 1 ===---\n");
+	
 	printf("Enter man's weight in kilo:\n");//height and 
 	scanf("%d", &w);
 
@@ -16,9 +22,21 @@ int main(void)
 
 	//Считаем индекс
 	index = w/(h*h);
-	printf("I=m/(h*h): %d\n", index);// 
+	printf("I=m/(h*h): %.2f\n", index);// 
+	*/
+	
+	//"Найти максимальное из четырех чисел. Массивы не использовать
+	printf("\n---=== 2 ===---\n");
+	printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
 
+	max = a;
+	if (max < b) max = b;
+	if (max < c) max = c;
+	if (max < d) max = d;
 
+	printf("max = %d\n", max);
+
+	scanf("%f", &h);
 
 	return 0;
 }
