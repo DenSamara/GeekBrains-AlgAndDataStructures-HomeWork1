36ae9358c6ca211114ca1ce35cae5fb74cbbd116
Denis Konovalov <den.konovaloff@gmail.com>
2018-09-25 15:44:26 +0400

    Убрал лишний код

--- Main.c
+++ Main.c
@@ -3,39 +3,7 @@
 
 int main(void)
 {
-	//Task 1
-	int w;
-	float h, index;
-
-	//Task 2
-	int a = 1, b = 2, c = 9, d = 3, max;
-
-	/*
-	//"Ввести вес и рост человека. Рассчитать и вывести индекс массы тела по формуле I=m/(h*h); где m-масса тела в килограммах, h - рост в метрах\n"
-	printf("---=== 1 ===---\n");
-	
-	printf("Enter man's weight in kilo:\n");//height and 
-	scanf("%d", &w);
-
-	printf("Enter man's height in meters:\n");// 
-	scanf("%f", &h);
-
-	//Считаем индекс
-	index = w/(h*h);
-	printf("I=m/(h*h): %.2f\n", index);// 
-	
-	
-	//"Найти максимальное из четырех чисел. Массивы не использовать
-	printf("\n---=== 2 ===---\n");
-	printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
-
-	max = a;
-	if (max < b) max = b;
-	if (max < c) max = c;
-	if (max < d) max = d;
-
-	printf("max = %d\n", max);
-	*/
+	int a, b, c;
 
 	//Написать программу обмена значениями двух целочисленных переменных:
 	//a. с использованием третьей переменной;
@@ -57,8 +25,6 @@ int main(void)
 	a = a - b;
 
 	printf("a = %d, b = %d\n", a, b);
-
-	scanf("%f", &h);
 
 	return 0;
 }
