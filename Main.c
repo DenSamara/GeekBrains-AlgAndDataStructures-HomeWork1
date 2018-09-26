8bfffc3114c329e002d8893a4ab4da35b9ebcc46
Denis Konovalov <den.konovaloff@gmail.com>
2018-09-26 11:38:50 +0400

    Task 9. Даны целые положительные числа N и K. Используя только операции сложения и вычитания, найти частное от деления нацело N на K, а также остаток от этого деления

--- Main.c
+++ Main.c
@@ -3,27 +3,31 @@
 
 int main(void)
 {
-	//Ввести a и b и вывести квадраты и кубы чисел от a до b
+	//Даны целые положительные числа N и K. Используя только операции сложения и вычитания, найти частное от деления нацело N на K, а также остаток от этого деления
 
-	int a, b, i;
+	int n, k;
+	//Частное и остаток
+	int quotient = 0, rest;
 
 	printf("\n---=== 8 ===---\n");
 	
 	do{
-		printf("Please input a: ");
-		scanf("%d", &a);
-		printf("Please input b: ");
-		scanf("%d", &b);
+		printf("Please input n: ");
+		scanf("%d", &n);
+		printf("Please input k: ");
+		scanf("%d", &k);
+		if (n < k) printf("n must be more than k\n");
+	}while (n < k);
 
-		//Можно не делать контроль ввода, а начать цикл с меньшего числа, например
-		if (a > b) printf("a must be more than b\n");
-	} while (a > b);
+	rest = n;
+	do{
+		rest -= k;
+		quotient++;
+	}while (rest >= k);
 
-	for (i = a; i <= b; i++){
-		printf("%d ^ 2 = %d, %d ^ 3 = %d\n", i, i*i, i, i*i*i);
-	}
 
+	printf("%d/%d = %d (%d)\n", n, k, quotient, rest);
+
+	scanf("%d", &n);
 	return 0;
 }
-
-
