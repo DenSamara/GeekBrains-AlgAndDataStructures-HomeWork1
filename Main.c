23de11518ed8c3784f91b1073e81177928fcfe6a
Denis Konovalov <den.konovaloff@gmail.com>
2018-09-26 12:56:34 +0400

    Task 11. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать среднее арифметическое всех положительных четных чисел, оканчивающихся на 8.

--- Main.c
+++ Main.c
@@ -3,34 +3,27 @@
 
 int main(void)
 {
-	//Дано целое число N (> 0). С помощью операций деления нацело и взятия остатка от деления определить,
-	//имеются ли в записи числа N нечетные цифры. Если имеются, то вывести True, если нет — вывести False.
+	//11. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать среднее арифметическое всех положительных четных чисел, оканчивающихся на 8.
 
-	int n;
-	//Частное и остаток
-	int result = 0, rest;
+	int n, a, i, sum;
 
-	printf("\n---=== 10 ===---\n");
-	
+	printf("\n---=== 11 ===---\n");
+	i = 0;
+	sum = 0;
 	do{
 		printf("Please input n: ");
 		scanf("%d", &n);
 		
-		if (n <= 0) printf("n must be more than 0\n");
-	}while (n <= 0);
-
-	do{
-		rest = n % 10;
-		//Если нашли нечётное число - выходим
-		if (rest % 2 == 1){
-			result = 1;
-			break;
+		if ((n - div(n, 10) * 10) == 8){
+			sum += n;
+			i++;
 		}
+	}while (n != 0);
 
-		n = div(n-rest, 10);
-	}while (n > 0);
-
-	printf(result == 1 ? "true" : "false");
+	if (i > 0)
+		printf("Was entered numbers end with 8: %d, avg is %d\n", i, sum/i);
+	else
+		printf("Was entered zero numbers ends with 8\n");
 
 	scanf("%d", &n);
 	return 0;
