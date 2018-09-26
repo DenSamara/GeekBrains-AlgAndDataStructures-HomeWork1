5ca7ea7fd9c5c3e88cb4b8ba8eebc626b84791ea
Denis Konovalov <den.konovaloff@gmail.com>
2018-09-26 11:24:04 +0400

    Task 8. Ввести a и b и вывести квадраты и кубы чисел от a до b

--- Main.c
+++ Main.c
@@ -3,50 +3,25 @@
 
 int main(void)
 {
-	//С клавиатуры вводятся числовые координаты двух полей шахматной доски (x1,y1,x2,y2)
-	//Требуется определить, относятся ли поля к одному цвету или нет
+	//Ввести a и b и вывести квадраты и кубы чисел от a до b
 
-	int x1, y1, x2, y2, c1, c2;
+	int a, b, i;
 
-	printf("\n---=== 7 ===---\n");
-	//Берём координаты 1
-	printf("Please input x1: ");
-	scanf("%d", &x1);
-	printf("Please input y1: ");
-	scanf("%d", &y1);
+	printf("\n---=== 8 ===---\n");
+	
+	do{
+		printf("Please input a: ");
+		scanf("%d", &a);
+		printf("Please input b: ");
+		scanf("%d", &b);
 
-	//Берём координаты 2
-	printf("Please input x2: ");
-	scanf("%d", &x2);
-	printf("Please input y2: ");
-	scanf("%d", &y2);
+		//Можно не делать контроль ввода, а начать цикл с меньшего числа, например
+		if (a > b) printf("a must be more than b\n");
+	} while (a > b);
 
-	printf("x1 = %d, y1 = %d\nx2 = %d, y2 = %d\n", x1, y1, x2, y2);
-
-	if (x1 == y1 || (x1 % 2 == 1 && y1 % 2 == 1)){
-		printf("Colour of first poinf is black\n");
-		c1 = 0;
+	for (i = a; i <= b; i++){
+		printf("%d ^ 2 = %d, %d ^ 3 = %d\n", i, i*i, i, i*i*i);
 	}
-	else {
-		printf("Colour of first poinf is white\n");
-		c1 = 1;
-	}
-
-	if (x2 == y2 || (x2 % 2 == 1 && y2 % 2 == 1)){
-		printf("Colour of second poinf is black\n");
-		c2 = 0;
-	}
-	else {
-		printf("Colour of second poinf is white\n");
-		c2 = 1;
-	}
-
-	if (c1 == c2)
-		printf("Colours is the same");
-	else
-		printf("Colours are different");
-
-	scanf("%d", &y2);
 
 	return 0;
 }
